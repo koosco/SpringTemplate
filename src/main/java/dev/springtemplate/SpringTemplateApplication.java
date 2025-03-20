42b930c8cf3f2e8e1eef4e3d40b247a3fa2220fd
mango <mango.koo@goorm.io>
2025-03-20 18:13:44 +0900

    refactor: simplify package structure and update group name

--- src/main/java/dev/springtemplate/SpringTemplateApplication.java
+++ src/main/java/dev/springtemplate/SpringTemplateApplication.java
@@ -1,4 +1,4 @@
-package custom_memo.dev.springtemplate;
+package dev.springtemplate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
