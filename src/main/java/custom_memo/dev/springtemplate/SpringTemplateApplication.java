a003363bda6cd0122d7d411606b8c42a9caa0d6c
mango <mango.koo@goorm.io>
2025-03-20 16:37:49 +0900

    initial commit

---
+++ src/main/java/custom_memo/dev/springtemplate/SpringTemplateApplication.java
@@ -0,0 +1,13 @@
+package custom_memo.dev.springtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/custom_memo/dev/springtemplate/SpringTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package custom_memo.dev.springtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
